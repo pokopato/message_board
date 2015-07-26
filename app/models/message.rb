525b01a945a97bd2edb25392be2be747243ba001
pokopato <patora20140808@gmail.com>
2015-07-26 12:28:13 +0000

    finish challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :age, numericality: { less_than: 120, greater_than: 0, only_integer: true }
 end
